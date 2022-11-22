0e98f2c8005038d2ab5bd0062f6f4542970b1a19
PabloAza89 <juanpabloazambuyo@gmail.com>
2022-11-22 03:17:05 -0300

    h

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,8 +7,9 @@ import { Link } from "react-router-dom";
 export default function Form({ GetAfterCreated }) {
   
   const [showAlert, setShowAlert] = useState(false)
+  const [firstInstance, setFirstInstance] = useState(false)
 
-  let [created, setCreated] = useState(0)
+  const [created, setCreated] = useState(0)
   const [dietSelected, setDietSelected] = useState([])
   let uniqueNamesDiets = Array.from(new Set(dietSelected));
 
@@ -31,14 +32,15 @@ export default function Form({ GetAfterCreated }) {
     instructions: ""
   });
 
-  function handleNewRecipe() {
+  function handleNewRecipe() {    
     setDietSelected([])
     setCreated(0)
     setTitle("")
     setHealthScore("")
     setSummary("")
     setAnalyzedInstructions("")
-    setError(error, error.title = "", error.health.one = "", error.health.two = "", error.summary= "", error.instructions= "") 
+    setError(error, error.title = "", error.health.one = "", error.health.two = "", error.summary= "", error.instructions= "")
+    
   }
   
   function handleSubmitButton() {
@@ -70,10 +72,12 @@ export default function Form({ GetAfterCreated }) {
       .then(setCreated(1))
       .catch((err) => {
         if (err.message === "Unexpected token 'T', \"THERE WAS \"... is not valid JSON") setCreated(0)
-      });      
+      });
     } else {
       setShowAlert(true)
+      
     }
+    
  };
 
   function validateTitle(value) {
@@ -128,69 +132,83 @@ export default function Form({ GetAfterCreated }) {
       hidden: false
     })
   }
+ 
+  return !firstInstance ?
+    (
+      <div className="form-body">
+        <Link  id="iconImageDiv"  to="/" >
+          <img onClick={() => GetAfterCreated()} className="iconImageForm" src={logo} alt=""></img>
+        </Link>
+        <Link  id="iconText" to="/">
+          <h2 onClick={() => GetAfterCreated()} >Go Back !</h2> 
+        </Link>
+        <form className="form" onSubmit={handleSubmit}>        
+          <img className="image-form" src={noImage1} alt=""></img>
+          <div className="titleFormRecipe">Create your own recipe ! Please fill in all fields:</div>
+          <div className="options-main-align">
+            <div className="options-main-left">
+              <div className="options-left">Title:</div>
+              <div className="options-left">Health Score:</div>
+              <div className="options-left">Summary:</div>
+              <div className="options-left">Instructions:</div>
+              <div className="options-left">Diets:</div>
+            </div>
+            <div className="options-main-right">           
+              <input className='danger' id="checkerTitle" name="title" type="text" value={title} placeholder="e.g. Pasta.." onChange={(e) => validateTitle(e.target.value)  }/>
+              <input className='danger' id="checkerHealth" name="healthScore" type="text" value={healthScore} placeholder="e.g. 73" onChange={(e) => validateHealthScore(e.target.value)  }/>
+              <input className='danger' id="checkerSummary" name="summary" type="text" value={summary} placeholder="e.g. Healthy pasta recipe" onChange={(e) => validateSummary(e.target.value) }/>
+              <input className='danger' id="checkerInstructions" name="analyzedInstructions" type="text" value={analyzedInstructions} placeholder="e.g. Cut pasta, fry tomatoes.." onChange={(e) => validateAnalyzedInstructions(e.target.value) }/>
 
-  return !showAlert ?
-  (
-    <div className="form-body">
-      <Link  id="iconImageDiv"  to="/" >
-        <img onClick={() => GetAfterCreated()} className="iconImageForm" src={logo} alt=""></img>
-      </Link>
-      <Link  id="iconText" to="/">
-        <h2 onClick={() => GetAfterCreated()} >Go Back !</h2> 
-      </Link>
-      <form className="form" onSubmit={handleSubmit}>        
-        <img className="image-form" src={noImage1} alt=""></img>
-        <div className="titleFormRecipe">Create your own recipe ! Please fill in all fields:</div>
-        <div className="options-main-align">
-          <div className="options-main-left">
-            <div className="options-left">Title:</div>
-            <div className="options-left">Health Score:</div>
-            <div className="options-left">Summary:</div>
-            <div className="options-left">Instructions:</div>
-            <div className="options-left">Diets:</div>
+              <select id="choose" className="choose" onChange={event => handleDietSelected(event.target.value) + handleSubmitButton() } onClick={event => formHandler(event.target.value)  } >
+                <option id="-- select an option --" disabled={ dietChoosen.hidden ? true : false } >{dietChoosen.hidden && dietChoosen.name !== "-- select an option --" ?  "-- select an option --" : dietChoosen.name }</option>
+                <option >{ dietChoosen.hidden ? "Gluten Free" : "-- select an option --" }</option>
+                <option >{ dietChoosen.hidden ? "Ketogenic" : "-- select an option --" }</option>
+                <option >{ dietChoosen.hidden ? "Vegan" : "-- select an option --" }</option>
+                <option >{ dietChoosen.hidden ? "Lacto Ovo Vegetarian" : "-- select an option --" }</option>
+                <option >{ dietChoosen.hidden ? "Pescatarian" : "-- select an option --" }</option>
+                <option >{ dietChoosen.hidden ? "Paleolithic" : "-- select an option --" }</option>
+                <option >{ dietChoosen.hidden ? "Primal" : "-- select an option --" }</option>
+                <option >{ dietChoosen.hidden ? "Fodmap Friendly" : "-- select an option --" }</option>
+                <option >{ dietChoosen.hidden ? "Whole 30" : "-- select an option --" }</option>
+                <option >{ dietChoosen.hidden ? "Dairy Free" : "-- select an option --" }</option>
+              </select >
+            </div>
           </div>
-          <div className="options-main-right">           
-            <input className='danger' id="checkerTitle" name="title" type="text" value={title} placeholder="e.g. Pasta.." onChange={(e) => validateTitle(e.target.value)  }/>
-            <input className='danger' id="checkerHealth" name="healthScore" type="text" value={healthScore} placeholder="e.g. 73" onChange={(e) => validateHealthScore(e.target.value)  }/>
-            <input className='danger' id="checkerSummary" name="summary" type="text" value={summary} placeholder="e.g. Healthy pasta recipe" onChange={(e) => validateSummary(e.target.value) }/>
-            <input className='danger' id="checkerInstructions" name="analyzedInstructions" type="text" value={analyzedInstructions} placeholder="e.g. Cut pasta, fry tomatoes.." onChange={(e) => validateAnalyzedInstructions(e.target.value) }/>
-
-            <select id="choose" className="choose" onChange={event => handleDietSelected(event.target.value) + handleSubmitButton() } onClick={event => formHandler(event.target.value)  } >
-              <option id="-- select an option --" disabled={ dietChoosen.hidden ? true : false } >{dietChoosen.hidden && dietChoosen.name !== "-- select an option --" ?  "-- select an option --" : dietChoosen.name }</option>
-              <option >{ dietChoosen.hidden ? "Gluten Free" : "-- select an option --" }</option>
-              <option >{ dietChoosen.hidden ? "Ketogenic" : "-- select an option --" }</option>
-              <option >{ dietChoosen.hidden ? "Vegan" : "-- select an option --" }</option>
-              <option >{ dietChoosen.hidden ? "Lacto Ovo Vegetarian" : "-- select an option --" }</option>
-              <option >{ dietChoosen.hidden ? "Pescatarian" : "-- select an option --" }</option>
-              <option >{ dietChoosen.hidden ? "Paleolithic" : "-- select an option --" }</option>
-              <option >{ dietChoosen.hidden ? "Primal" : "-- select an option --" }</option>
-              <option >{ dietChoosen.hidden ? "Fodmap Friendly" : "-- select an option --" }</option>
-              <option >{ dietChoosen.hidden ? "Whole 30" : "-- select an option --" }</option>
-              <option >{ dietChoosen.hidden ? "Dairy Free" : "-- select an option --" }</option>
-            </select >
+          <div className="dietAlign">
+            {uniqueNamesDiets[0]&&"Diets choosen: "}{uniqueNamesDiets.map(function(e) {
+                              if ((uniqueNamesDiets.indexOf(e) !== uniqueNamesDiets.length - 1)) {
+                                  return e + " + "
+                              } else return e
+                              })}
           </div>
-        </div>
-        <div className="dietAlign">
-          {uniqueNamesDiets[0]&&"Diets choosen: "}{uniqueNamesDiets.map(function(e) {
-                            if ((uniqueNamesDiets.indexOf(e) !== uniqueNamesDiets.length - 1)) {
-                                return e + " + "
-                            } else return e
-                            })}
-        </div>
-        <input id="submmitButton" type="submit" disabled={handleSubmitButton()}  value="CREATE !" />
-        <input id="createNewButton" type="submit" onClick={() => handleNewRecipe() + createHandler ()} value="CREATE NEW RECIPE!" />  
-        {[error.title , error.health.one, error.health.two ,  error.summary , error.instructions].map(e => (
-          e?<span key={e} className="alert">{e}</span>:null
-        ))}
-      </form>
+          <input id="submmitButton" type="button" disabled={handleSubmitButton()} onClick={(event) => setFirstInstance(true) + handleSubmit(event)} value="CREATE !" />
+          <input id="createNewButton" type="submit" onClick={() => handleNewRecipe() + createHandler() + setFirstInstance(false)} value="CREATE NEW RECIPE!" />  
+          {[error.title , error.health.one, error.health.two ,  error.summary , error.instructions].map(e => (
+            e?<span key={e} className="alert">{e}</span>:null
+          ))}
+        </form>
       </div>
     )
+    : !showAlert ?
+    (
+      <div className="alertButtonBGSuccess">      
+        <span>RECIPE CREATED SUCCESSFULLY !</span>
+        <button className='alertButtonSuccess' id="success" onClick={() => setShowAlert(false) + setFirstInstance(false)}></button>
+        <div className="hiddenSuccess">
+          {
+            setTimeout(() => {
+              document.getElementById("success")&&document.getElementById("success").click()            
+            }, 1500)
+          }
+        </div>
+      </div>
+    )    
     :
     (
-    <div className="alertButtonBG">
-      <span>Recipe was already created ! </span>
-      <span>Please select 'CREATE NEW RECIPE!' to create a new one !</span>
-      <button className='alertButton' onClick={() => setShowAlert(false)}><b>GO BACK !</b></button>
-    </div>
+      <div className="alertButtonBG">
+        <span>Recipe was already created ! </span>
+        <span>Please select 'CREATE NEW RECIPE!' to create a new one !</span>
+        <button className='alertButton' onClick={() => setShowAlert(false) + setFirstInstance(false)}><b>GO BACK !</b></button>
+      </div>
     )
 }
